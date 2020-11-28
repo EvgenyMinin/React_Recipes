9e3cac8c7602f5a1c09f829b1a9ae962951a3832
EvgenyMinin <evgeny.frontend@gmail.com>
2020-11-28 13:00:46 +0300

    added styled-components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledHeader = styled.h1`
+  font-size: 2em;
+  text-align: left;
+  padding: 16px;
+`;
 
 const Index = () => {
   return (
     <div>
-      <p>Index Page</p>
+      <StyledHeader>Index Page</StyledHeader>
     </div>
   );
 };
