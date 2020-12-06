e0556cd3f0cd982ddf6e4814064bc47bc929215e
EvgenyMinin <evgeny.frontend@gmail.com>
2020-12-06 10:17:58 +0300

    added recipe page

---
+++ pages/recipe/[id].tsx
@@ -0,0 +1,56 @@
+import { useQuery } from "@apollo/client";
+import React from "react";
+import * as _ from "lodash";
+
+import MainLayout from "../../components/layout/MainLayout";
+import Error from "../../components/notify/Error";
+import Loading from "../../components/notify/Loading";
+
+import { recipeGraphQL } from "../../graphql/queries/recipe";
+import { IProps } from "./types";
+
+const Recipe = ({ id }: IProps) => {
+  const { loading, data, error } = useQuery(recipeGraphQL, {
+    variables: { where: { id } },
+  });
+
+  const title = _.get(data, "recipe.title");
+
+  if (loading) {
+    return (
+      <MainLayout title="Recipe loading">
+        <Loading />
+      </MainLayout>
+    );
+  }
+
+  if (error) {
+    return (
+      <MainLayout title="Recipe error">
+        <Error errorText={`${error}`} />
+      </MainLayout>
+    );
+  }
+
+  if (!title) {
+    return (
+      <MainLayout title="Not a valid recipe">
+        <Error errorText="Not a valid recipe" />
+      </MainLayout>
+    );
+  }
+
+  return (
+    <MainLayout title={title}>
+      <div>Recipe {id}</div>
+    </MainLayout>
+  );
+};
+
+Recipe.getInitialProps = ({ query }) => {
+  const { id } = query;
+
+  return { id };
+};
+
+export default Recipe;
